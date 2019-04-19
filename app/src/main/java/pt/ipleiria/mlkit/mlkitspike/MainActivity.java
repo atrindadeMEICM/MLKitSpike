651ac336b30c68ffb628bebe0070b7a38ead7e91
Alberto <lostheaven92@gmail.com>
2019-04-19 15:54:10 +0100

    Initial commit

---
+++ app/src/main/java/pt/ipleiria/mlkit/mlkitspike/MainActivity.java
@@ -0,0 +1,42 @@
+package pt.ipleiria.mlkit.mlkitspike;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button btnFaces;
+    private Button btnGallery;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Bind views
+        btnFaces = findViewById(R.id.btn_faces);
+        btnGallery = findViewById(R.id.btn_gallery);
+        
+        btnFaces.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, FacesActivity.class);
+                startActivity(intent);
+//                Toast.makeText(MainActivity.this, "Faces", Toast.LENGTH_SHORT).show();
+            }
+        });
+        
+        btnGallery.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, GalleryActivity.class);
+                startActivity(intent);
+//                Toast.makeText(MainActivity.this, "Landscapes", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
